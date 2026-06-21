55e4a93afb58426964e4df93771bb4c941257739
Flora Chan <flora.chan010@gmail.com>
2026-06-21 22:13:05 +1000

    added in cookies for sign in

--- convention_sign_in.js
+++ convention_sign_in.js
@@ -1,11 +1,18 @@
 const signInBtn = document.getElementById("sign-in-button")
+const signOutBtn = document.getElementById("sign-out-button")
 const signInModal = document.getElementById("sign-in-modal")
 const modalClose = document.getElementById("modal-close-btn")
 const signInForm = document.getElementById("sign-in-form")
 
-// show sign in form when "sign in" button clicked
+// show sign in form when "sign in" button clicked (when not signed in)
+// otherwise the sign in button will become a sign out button
 signInBtn.addEventListener("click", () => {
     signInModal.style.display = 'inline'
+});
+
+signOutBtn.addEventListener("click", () => {
+    deleteAllCookies();
+    toggleSignInOut();
 });
 
 // stop showing form when x is clicked
@@ -34,8 +41,9 @@ signInForm.addEventListener('submit', function(e){
             console.log("could not sign in")
             // create new account in accounts.json
         }
+        toggleSignInOut();
+        signInModal.style.display = 'none';
     })
-    signInModal.style.display = 'none'
 })
 
 // Function to get a cookie by name
@@ -49,3 +57,27 @@ function getCookie(name) {
   }
   return null; // Return null if cookie not found
 }
+
+// Function to delete all cookies
+function deleteAllCookies() {
+    // Beware! If your cookies are configured to use a path or domain component, this handy snippet won't work. (currently not configured with path)
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
+}
+
+function toggleSignInOut() {
+    if (getCookie("signedin") === "true") {
+        // show sign out button
+        signOutBtn.style.display = 'inline'
+        signInBtn.style.display = 'none'
+    } else {
+        // show sign in button
+        signOutBtn.style.display = 'none'
+        signInBtn.style.display = 'inline'
+    }
+}
+
+document.addEventListener("load", toggleSignInOut());
